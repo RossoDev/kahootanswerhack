cbe511eb9b5852d0b6829cea93f176f1986ccbdb
RossoDev <44946921+RossoDev@users.noreply.github.com>
2023-02-07 08:54:46 +0100

    Add files via upload

---
+++ Kahoot Answer Hack.bat
@@ -0,0 +1,200 @@
+@echo off
+REM NEXT
+:next
+cls
+
+:loop
+Set /a num=(%Random% %%9)+1
+color %num%
+
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&&#BBGGGGGGPPPPPPPPGGGGBBB#&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "@@@@@@@@@@@@@@@@@@@@@@@&###G5YJ?JJY5PGB##&&&&&&&&&&&&##BGPPJ!77JY5GGB##&@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "@@@@@@@@@@@@@@@@@@@@B5??7777YPGB&@@@@@@@@@&P@@@@@@@@@@@@@GJ~!B57~7!7?JJY55PBB#@@@@@@@@@@@@@@@@@@@@@@"
+echo "@@@@@@@@@@@@@@@@@&P?7!!?YPB&@@@@@@&#@@@@@@JJ&@@&@@&BPJ?JJ7J77##G5~^^^~7?JYY5YJJYB&@@&#@@@@@@@@@@@@@@"
+echo "@@@@@@@@@@@@@&@GY7~~!?P#@@@@@@@@@@@Y&@@&BYYY?5GJ5P55555J7JG?~!7~~~~~!77777?JY55YJ?J5J?5B&&@@@@@@@@@@"
+echo "@@@@@@@@@@#GY7?77~!JG&@@@@@@@@@#BP5JPGYJ?5#GP5PPG#&&##BGBGGGGP5555YY?77777777?JYJJYJ?!^~?P5B&@@@@@@@"
+echo "@@@@@@@&BPJ7~~!77JB@@@@@@@@#Y7??~^^!YP55G##BB##B##&##BBB#&&##&&@&#BPYJ?????JJYYYY55555J7!7^~JB&@@@@@"
+echo "@@@@&@&BP?7!!?!7B@@@@@@@@&#5!^!!~^^~?G#&######BBB###&#&####BBBBGGBGBBGGPPP55YY5555555555PY7~~5@@@@@@"
+echo "@@@@@#YYY~^!7!5&@@@@@@&5PYJYYJ?7!~^JBBBBGBGGGGGGGBBBBB#BBB###B#&#BBGGBB###G5YY555555Y5555P5Y?7P@@@@@"
+echo "@@@@&5!?!~7??B@@@@@&#GP5!^~7J?!7?7JPGP5PPPGGGGBBGPPGPGBBBB##BB#B##&#BPPG#BPB#BGGP5555YY555555J7P@@@@"
+echo "@@&GPG7!7?5P@@@@@@#J?55Y?!7?J7!???Y5BB##BPB#GPPPGBGPPPPBB#B#&B##BB##G!^!J?!?J5#BP5555YYYYYJ?YJJB@@@@"
+echo "@B5GG!7P#&@@@@@#PJ7?JJJPJ??J???!!J&@@@@@@@@#BGG&@@@@&BPGB##&&#&&##G##J::^~~~~JBBGGGPP55555YJ??5P&@@@"
+echo "@&G&@P#@@@@@B5?7!7JJ77??7!7YY77!~G@@@@@@@@@@&B@@@@@@@@#5PP5GGBB#BBPB#P~^^^^^~~!5##B#####GPYYJYYB@@@@"
+echo "@@@@@@@@&B5??JYJ?777JJ???77JJJ?!~P@@@@@@@&#BPP@@@@@@@@BYYJJYYY555GG##P~^:^^^~~^~GBGGGPGGGGGGGPY5B&@@"
+echo "@@@@@&P?77J5GBBPJ!75J?7??????777!75B@&#BPYJJJYP#&&&&B5J?JJYYJYYPGGPB#5~^~~~~~~!JY5GP55GG5???J??J5G@@"
+echo "@@@@@5~!JG#&&#G??!JP???777!!!!77!7JYPGG55YYY5YJYYYYYYJ?JYYYYYYYYPPPB#Y7!!!!!!!!7~^~?JJ#BG5Y!J5JJ5B@@"
+echo "@@@@G!!7YBBBBGG??J?Y77?77!~!!!!~~~?P55PPGPPG5555Y5YYYYYJJJJY55555P##Y!??!~~!~~77~^^~~~~~!7775#&@@@@@"
+echo "@@@@#YJ?7?Y5PPP5Y?!5PJ?!!!!!~^^^^^~!YPY5PPG5PPPPPGPP55YJJJY5PG55G##Y~~??7!77~^!!~~~!777?JJJ?7??P#@@@"
+echo "@@@@@@@&&BGPGGGGPP5J5?!!!!!~::^^^~^^~?PPYY55PGPPPPPP55P555PPPPGB#G7^^~!7??JJ?!!!?JJ??JY????!~!?J5B@@"
+echo "@@@@@@@@@@@@@@@@@@@@@#BP?P#GJ?J!^^77~~!J5P555PPGPPPPPPGPGPPPGBBGY!^~~777!!YY?JJ?J????JJJ?!~^7P#@&#@@"
+echo "@@@@@@@@@@@@@@@@@@@@@@@&#@@@@@&BJ!7JJJ?!YPPGGGPPPPPPPGGGGGBBGGPPPG5JY5?7!!7???JYJJ???77?YJPYB&&@@@@@"
+echo "@@@@@@@@@@@@@@@@@@@@@@@&@@&@@@@@#J7777Y&@B#BPYJBGB5Y??YGB#BPBGP5PPYYJ7!?JJJ????J77!??Y5G#G#@@@@&&@@@"
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@5!?7~?##GG#BG#BBPY57???YJJJJ7~!777JJ?7?7J?7!!JY5G#B@@@&@#B&@@@@@@@@"
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&#@B5BG!!#@@@@&#PPPG&##GP?Y7JGY5GGB5?5?JYYYJJP@@@@@@@@@@@@@@@@@@@@@"
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&G&@@@@@@@@@@@@@@@PPJ?@@@@#BB5Y5#@&@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&@@@##GJ#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "=> https://github.com/RossoDev/ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo " ____  __.      .__                   __       _____                                       "
+echo "|    |/ _|____  |  |__   ____   _____/  |_    /  _  \   ____   _______  _  __ ___________  "
+echo "|      < \__  \ |  |  \ /  _ \ /  _ \   __\  /  /_\  \ /    \ /  ___| \/ \/ // __ \_  __ \ "
+echo "|    |  \ / __ \|   Y  (  <0> |  <0> )  |   /    |    \   |  \\___ \ \     /\  ___/|  | \/ "
+echo "|____|__ (____  /___|  /\____/ \____/|__|   \____|__  /___|  /____  > \/\_/  \___  >__|    "
+echo "        \/    \/     \/                             \/     \/     \/             \/        "
+echo "  ___ ___    _____  _________  ____  __."
+echo " /   |   \  /  _  \ \_   ___ \|    |/ _|"
+echo "/    ~    \/  /_\  \/    \  \/|      <  "
+echo "\    Y    /    |    \     \___|    |  \ "
+echo " \___|_  /\____|__  /\______  /____|__ \"
+echo "       \/         \/        \/        \/"
+echo "=> Proudly by RossoDev                  "
+echo "=> Pure batch, for u <3                 "
+echo:
+echo  [1] START
+echo  [2] USAGE
+echo  [3] LINKS
+echo  [4] EXIT
+
+CHOICE /C 1234 /M "Enter your choice:"
+
+IF ERRORLEVEL 4 GOTO exi
+IF ERRORLEVEL 3 GOTO social
+IF ERRORLEVEL 2 GOTO usage
+IF ERRORLEVEL 1 GOTO app
+
+REM EXIT
+:exi
+exit
+
+REM SOCIAL
+:social
+
+cls
+echo:
+echo  [1] GITHUB
+echo  [2] MAIN
+
+CHOICE /C 12 /M "Enter your choice:"
+
+IF ERRORLEVEL 2 (
+    GOTO loop)
+IF ERRORLEVEL 1 (
+    START CHROME https://github.com/RossoDev/)
+
+goto social
+
+REM USAGE
+:usage
+color a
+cls
+
+echo INSTRUCTION MANUAL:
+echo:
+echo Requirements:
+echo - Internet connection
+echo - JQ [OPTIONAL] (library that will be used to allow a better reading of the converted json file)
+echo - Permissions to modify the Desktop folder (used to create and delete the file with Kahoot solutions)
+echo - UUID code (obtainable after "quizId=" from Kahoot URL, ex.
+echo https://play.kahoot.it/v2/?quizId=cfc981a8-cb08-4a0f-ac33-a1c59cc106ca)
+echo:
+echo How to use:
+echo 1. Press 1 to start
+echo 2. Put UUID code in the field
+echo 3. If it is correct, you will be able to see all the solutions of your kahoot!
+echo:
+echo Warning:
+echo When you return to the main page, the Kahoot answers document will be deleted.
+echo ____________________________________
+echo UUID EXAMPLES:
+echo cfc981a8-cb08-4a0f-ac33-a1c59cc106ca
+echo 285524a1-dd39-416a-b92e-2ccdf6d1b9d4
+echo c412a20b-bd57-4639-849b-d21ff8d7342b
+echo e084148b-5664-4102-bfe8-9ffbf9c15a25
+echo f55d2045-da78-4287-9203-6f7f39e32dd7
+
+pause
+
+goto next
+
+REM APP
+:app
+color a
+cls
+
+set /p input=UUID: 
+echo UUID = %input%, correct? [Y/N]
+set /p affermaid=
+if /i %affermaid%==y goto process ELSE FALSE goto falseid
+
+REM FALSE
+:falseid
+cls
+
+echo:
+echo  [1] RETYPE UUID CORRECTLY
+echo  [2] RETURN MAIN
+
+CHOICE /C 12 /M "Enter your choice:"
+
+IF ERRORLEVEL 2 GOTO next
+IF ERRORLEVEL 1 GOTO app
+pause
+
+REM PROCESS
+:process
+cls 
+
+set url=https://play.kahoot.it/rest/kahoots/%input%
+set filename=kahoot.txt
+
+curl.exe -o %filename% -k %url%
+
+findstr "errorCode" %filename% >NUL 2>&1
+cls
+
+if not errorlevel 1 (goto errorid) else (goto solution)
+
+REM ERRORID
+:errorid
+del %filename%
+
+echo No quiz found.
+timeout 2 > NUL
+
+goto next
+
+REM SOLUTION
+:solution
+cls
+
+if exist C:\ProgramData\chocolatey\lib\jq\tools\jq.exe (
+    jq . %filename%
+    echo:
+    echo TIME: %time%
+) else (
+    echo: >> %filename%
+    echo: >> %filename%
+    echo TIME: %time% >> %filename%
+    start notepad %filename%
+    echo The text file has been opened.
+)
+
+pause
+
+goto return
+
+REM RETURN
+:RETURN
+color c
+cls
+
+echo Would you like to return to the main screen? [Y/N]
+
+set /p retrn=
+if /i %retrn%==y (
+    del %filename%
+    goto next
+) ELSE FALSE (
+    color a
+    goto solution
+)
